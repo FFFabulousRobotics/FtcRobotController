d4df22b2caf6cc1fe799197460360983f709b77a
LeoLBWK <leo197612@163.com>
2024-09-15 23:21:16 +0800

    Add new ManualOpMode structure

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOpMode.java
@@ -1,21 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
-public class ManualOpMode extends OpMode
+public class ManualOpMode extends LinearOpMode
 {
 
-
-
-    @Override
-    public void init() {
-
-    }
+    DcMotor motor;
 
     @Override
-    public void loop() {
+    public void runOpMode() throws InterruptedException
+    {
+
+        motor = hardwareMap.dcMotor.get("LeftRightmotor");
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        waitForStart();
+        motor.setPower(1);
+        motor.setTargetPosition(1000);
 
     }
 }
