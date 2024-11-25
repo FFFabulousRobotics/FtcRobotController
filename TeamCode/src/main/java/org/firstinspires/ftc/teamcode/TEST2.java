74ab281291240d81d3f7d3a3d68e186878239766
Ehicy <tingxuan_deng2007@163.com>
2024-11-25 21:12:40 +0800

    1111

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEST2.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.hardware.RobotVision;
+
+@TeleOp
+
+public class TEST2 extends LinearOpMode {
+    private RobotVision robotVision;
+
+    @Override
+    public void runOpMode() {
+        robotVision = new RobotVision();
+
+        robotVision.initialize(hardwareMap); // 初始化摄像头
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // 显示颜色检测结果
+            telemetry.addData("Center Color", robotVision.getDetectedColor());
+            telemetry.update();
+
+            sleep(100);
+        }
+    }
+}
+
+
