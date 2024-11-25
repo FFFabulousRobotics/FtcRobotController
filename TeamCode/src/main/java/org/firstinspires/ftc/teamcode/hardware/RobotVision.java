84bed6c4d4cd1f0a6a93d414c91b821ec49480d9
Ehicy <tingxuan_deng2007@163.com>
2024-11-25 13:32:18 +0800

    这是第二版视觉识别,可以识别摄像头中心的颜色 robotVision.getDetectedColor()返回的是一个字符串然后这个字符串是四个类型Unknown,Red，Blue，Yellow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotVision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/RobotVision.java
@@ -1,13 +1,12 @@
-//这是第一版视觉识别,可以识别摄像头中心的颜色
-//robotVision.getDetectedColor()返回的是一个字符串然后这个字符串是三个类型Unknown,Red，Blue
-//
+//这是第二版视觉识别,可以识别摄像头中心的颜色
+//robotVision.getDetectedColor()返回的是一个字符串然后这个字符串是四个类型Unknown,Red，Blue，Yellow
+
 
 package org.firstinspires.ftc.teamcode.hardware;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
-import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
@@ -16,7 +15,6 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-
 
 public class RobotVision {
     private OpenCvCamera webcam;
@@ -51,10 +49,13 @@ public class RobotVision {
         Mat hsv = new Mat();
         Mat maskRed = new Mat();
         Mat maskBlue = new Mat();
+        Mat maskYellow = new Mat(); // 增加黄色检测的mask
         Scalar lowerRed = new Scalar(0, 100, 100);
         Scalar upperRed = new Scalar(10, 255, 255);
         Scalar lowerBlue = new Scalar(110, 100, 100);
         Scalar upperBlue = new Scalar(130, 255, 255);
+        Scalar lowerYellow = new Scalar(20, 100, 100); // 黄色的HSV范围
+        Scalar upperYellow = new Scalar(30, 255, 255);
 
         @Override
         public Mat processFrame(Mat input) {
@@ -64,17 +65,21 @@ public class RobotVision {
             Rect centerRect = new Rect(input.width() / 2 - 20, input.height() / 2 - 20, 40, 40);
             Mat centerMat = hsv.submat(centerRect);
 
-            // 检测红色和蓝色
+            // 检测红色、蓝色和黄色
             Core.inRange(centerMat, lowerRed, upperRed, maskRed);
             Core.inRange(centerMat, lowerBlue, upperBlue, maskBlue);
+            Core.inRange(centerMat, lowerYellow, upperYellow, maskYellow);
 
             double redValue = Core.mean(maskRed).val[0];
             double blueValue = Core.mean(maskBlue).val[0];
+            double yellowValue = Core.mean(maskYellow).val[0];
 
-            if (redValue > blueValue) {
+            if (redValue > blueValue && redValue > yellowValue) {
                 detectedColor = "Red";
-            } else if (blueValue > redValue) {
+            } else if (blueValue > redValue && blueValue > yellowValue) {
                 detectedColor = "Blue";
+            } else if (yellowValue > redValue && yellowValue > blueValue) {
+                detectedColor = "Yellow";
             } else {
                 detectedColor = "Unknown";
             }
@@ -83,4 +88,3 @@ public class RobotVision {
         }
     }
 }
-
