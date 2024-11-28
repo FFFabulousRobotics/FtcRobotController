451788fb76f89eb469dea448c61b5a65c27d68c1
Ehicy <tingxuan_deng2007@163.com>
2024-11-28 20:54:34 +0800

    third commit to improve the vision

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TEST3.java
@@ -0,0 +1,34 @@
+//测试摄像头角度识别功能
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.hardware.RobotVision2;
+
+@TeleOp
+
+
+
+public class TEST3 extends LinearOpMode {
+    private RobotVision2 robotVision2;
+
+    @Override
+    public void runOpMode() {
+        robotVision2 = new RobotVision2();
+
+        robotVision2.initialize(hardwareMap); // 初始化摄像头
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // 显示角度检测结果
+            telemetry.addData("Detected Angle", robotVision2.getDetectedAngle());
+            telemetry.update();
+
+            sleep(100);
+        }
+    }
+}
