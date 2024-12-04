f17672a0b953c60cc706464fd9742a1f5ae1f499
LeoLBWK <leo197612@163.com>
2024-12-04 20:07:32 +0800

    Feat:Fixed RR problems.The next step is to make the stability

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/SparkFunOTOSTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/SparkFunOTOSTest.java
@@ -1,17 +1,18 @@
 package org.firstinspires.ftc.teamcode.test;
 
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.I2cDevice;
 
-public class SparkFunOTOS {
-    private I2cDevice otosDevice;
+public class SparkFunOTOSTest {
+    private SparkFunOTOS otosDevice;
     private double x;
     private double y;
     private double heading;
 
-    public SparkFunOTOS(HardwareMap hardwareMap) {
+    public SparkFunOTOSTest(HardwareMap hardwareMap) {
         // 假设 OTOS 传感器连接到 I2C 端口
-        otosDevice = hardwareMap.get(I2cDevice.class, "otos");
+        otosDevice = hardwareMap.get(SparkFunOTOS.class, "otos");
         // 初始化传感器（具体初始化代码取决于 OTOS 库的实现）
         this.x = 0.0;
         this.y = 0.0;
