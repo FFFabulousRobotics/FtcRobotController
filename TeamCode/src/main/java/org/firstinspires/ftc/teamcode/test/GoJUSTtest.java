094ef49b36b5b58b2b571f3df4cdfa2d5d6bf65d
Eternity0 <eternity211@126.com>
2025-02-15 21:53:14 +0800

    sort the files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/GoJUSTtest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/GoJUSTtest.java
@@ -4,7 +4,6 @@ import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.SampleAuto;
 import org.firstinspires.ftc.teamcode.hardware.RobotAuto;
 import org.firstinspires.ftc.teamcode.hardware.RobotTop;
 @Autonomous
