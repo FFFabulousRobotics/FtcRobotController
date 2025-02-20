1ad86f5e4e23404291cedbc8588f593115a9e383
LeoLBWK <leo197612@163.com>
2025-02-20 22:31:00 +0800

    Feat:Fixed Autonomous again and again.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HangAuto3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HangAuto3.java
@@ -18,8 +18,8 @@ import org.firstinspires.ftc.teamcode.hardware.RobotTop;
 public class HangAuto3 extends LinearOpMode {
     final int STRETCH_BACK_POSITION = 70;
     final int STRETCH_OUT_POSITION = 1500;
-    final double SPIN_DEFAULT_POSITION_L = 0.85;
-    final double SPIN_DEFAULT_POSITION_R = 0.15;
+    final double SPIN_DEFAULT_POSITION_L = 0.54;
+    final double SPIN_DEFAULT_POSITION_R = 0.51;
     final double SPIN_HOVERING_POSITION_L = 0.25;
     final double SPIN_HOVERING_POSITION_R = 0.78;
     final double SPIN_DOWN_POSITION = 0;
@@ -34,13 +34,14 @@ public class HangAuto3 extends LinearOpMode {
     final double BACK_CLOSE = 0.7;
     final double BACK_OPEN = 0.1;
 
-    final private double[] posHanging = {14, -30, 0};//挂的地方
-    final private double[] posHanging2 = {-12, 30, 0};//第二次挂的地方(有误差)
-    final private double[] posHanging3 = {-16, -30, 0};//第三次挂的地方(有误差)
-    final private double[] posMiddleStop = {-20, -10, 0};//中间停靠点
-    final private double[] posReadyForPush = {-24, -43, 90};//准备推的位置
-    final private double[] posPushed = {-29, -4, 90};//推到这个位置
-    final private double[] posGet = {-27,-2,180};//拿方块的地方
+    final private double[] posHanging = {10.88, -30.5, 0};//挂的地方
+    final private double[] posHanging2 = {17, -27, 0};//第二次挂的地方(有误差)
+    final private double[] posHanging3 = {22, -27, 0};//第三次挂的地方(有误差)
+    final private double[] parkPosition = {-46.0, 0.0, 180};//停靠
+    final private double[] posMiddleStop = {-22, -5, 90};//中间停靠点
+    final private double[] posReadyForPush = {-17.75, -53.89, 90};//准备推的位置
+    final private double[] posPushed = {-27.65, -5.33, 90};//推到这个位置
+    final private double[] posGet = {-21.4,-0.5,180};//拿方块的地方
 
     RobotTop robotTop;
     RobotAuto robotAuto;
@@ -58,6 +59,8 @@ public class HangAuto3 extends LinearOpMode {
         hangSample2();
         getSample();
         hangSample3();
+        getSample();
+        park();
     }
     protected void hangSample1(){
         ParallelCommandGroup cmd1 = new ParallelCommandGroup(
@@ -85,10 +88,10 @@ public class HangAuto3 extends LinearOpMode {
                 new GotoPosWithHeadingCommand(robotAuto,posReadyForPush[0],posReadyForPush[1],posReadyForPush[2],true),
                 new GotoPosWithHeadingCommand(robotAuto,posPushed[0],posPushed[1],posPushed[2],true),
                 new GotoPosWithHeadingCommand(robotAuto,posGet[0],posGet[1],posGet[2]),
-                new SleepCommand(300),
-                new ForwardCommand(robotAuto,-15),
+                new ForwardCommand(robotAuto,-10),
                 new InstantCommand(robotAuto::grab),
-                new ForwardCommand(robotAuto,1)
+                new ForwardCommand(robotAuto,1),
+                new SleepCommand(500)
         );
         cmd3.runCommand();
     }
@@ -119,10 +122,10 @@ public class HangAuto3 extends LinearOpMode {
         SequentialCommandGroup cmd5 = new SequentialCommandGroup(
                 new InstantCommand(() -> robotTop.setLiftPower(0)),
                 new GotoPosWithHeadingCommand(robotAuto,posGet[0],posGet[1],posGet[2]),
-                new ForwardCommand(robotAuto,-15),
+                new ForwardCommand(robotAuto,-10),
                 new InstantCommand(robotAuto::grab),
                 new ForwardCommand(robotAuto,1),
-                new SleepCommand(300)
+                new SleepCommand(500)
         );
         cmd5.runCommand();
     }
@@ -148,4 +151,11 @@ public class HangAuto3 extends LinearOpMode {
         );
         cmd2.runCommand();
     }
+    protected void park(){
+        SequentialCommandGroup cmd7 = new SequentialCommandGroup(
+                new GotoPosWithHeadingCommand(robotAuto,parkPosition[0],parkPosition[1],parkPosition[2])
+        );
+        cmd7.runCommand();
+    }
 }
+
